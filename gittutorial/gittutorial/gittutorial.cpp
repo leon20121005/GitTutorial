a7887ffa543da59205b75d347cb9bac6f54ea7b6
leon20121005 <leon20121005@gmail.com>
2017-02-24 11:53:42 +0800

    second commit

--- gittutorial/gittutorial/gittutorial.cpp
+++ gittutorial/gittutorial/gittutorial.cpp
@@ -3,9 +3,19 @@
 #include "stdafx.h"
 #include <string>
 #include <iostream>
+
 using namespace System;
-bool Guess(int number) {
+
+bool Guess(int number)
+{
+	return false;
 }
+
 int main(array<System::String ^> ^args)
 {
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
 }
