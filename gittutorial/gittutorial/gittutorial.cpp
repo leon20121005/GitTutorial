bcae4d27f4b69d4718b04a8f996b21eba8f3d828
asweetapple <sweetapplein1996@gmail.com>
2017-02-24 11:43:30 +0800

    initial commit

---
+++ gittutorial/gittutorial/gittutorial.cpp
@@ -0,0 +1,11 @@
+// gittutorial.cpp: 主要專案檔。
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace System;
+bool Guess(int number) {
+}
+int main(array<System::String ^> ^args)
+{
+}
